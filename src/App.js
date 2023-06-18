8aa24354dcb637aae630572b315acc1022debe96
RAHUL-HACKER-HACKER <rahulk841501@gmail.com>
2023-06-18 16:49:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    //FILE JSX -> JS+HTML
+    //IMPORTANT WE CAN WRITE ALL CODE IN PARENT FILE
+    <div class="card">
+  
+
+    <h1><b>Responsive Paragraph Word Counter</b></h1> 
+    <textarea id="w3review" name="w3review" rows="10" cols="80"></textarea><br></br>
+    <h3>Word Count:</h3>
+     
+  
+</div>
+  );
+}
+
+export default App;
